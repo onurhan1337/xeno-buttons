c9a08f2aba4d26517cdf1436ba80a18c37b5bb7b
Onurhan1337 <onurhandtr@gmail.com>
2022-05-20 18:38:30 +0300

    Initial Commit & beta 0.1.0

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <AnimatedButton msg="Like" />
+</template>
+
+<script>
+import AnimatedButton from "@/components/AnimatedButton";
+export default {
+  name: "App",
+  components: { AnimatedButton },
+};
+</script>
---
+++ src/components/AnimatedButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="animatedButton">{{ msg }}</button>
+</template>
+
+<script>
+export default {
+  name: "AnimatedButton",
+  props: {
+    msg: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.animatedButton {
+  background-color: deeppink;
+  border: none;
+  padding: 1rem;
+  color: antiquewhite;
+  font-size: 1rem;
+}
+</style>
